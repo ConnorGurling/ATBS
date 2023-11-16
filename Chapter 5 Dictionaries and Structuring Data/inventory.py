a1164fda7bb4a61aa25ca3c31221e928d101debd
ConnorGurling <Cgurling@googlemail.com>
2023-11-16 11:35:16 +0000

    inventory part 2

--- Chapter 5 Dictionaries and Structuring Data/inventory.py
+++ Chapter 5 Dictionaries and Structuring Data/inventory.py
@@ -1,12 +1,21 @@
-# inventory.py
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+def add_to_inventory(inventory, added_items):
+    for item in added_items:
+        if item in inventory.keys():
+            inventory[item] = inventory[item] + 1
+        else:
+            inventory[item] = 1
+    return inventory
 
-def displayInventory(inventory):
+inv = {'gold coin': 42, 'rope': 1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = add_to_inventory(inv, dragon_loot)
+
+def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
         print(f"{v} {k}")
         item_total += v
-    print("Total number of items: " + str(item_total))
+    print(f"\nTotal number of items: {item_total}")
 
-displayInventory(stuff)
+display_inventory(inv)
