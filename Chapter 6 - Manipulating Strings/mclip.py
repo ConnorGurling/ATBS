7880b9b4d2e0dd8e9ef2881174953cf4a3439271
ConnorGurling <Cgurling@googlemail.com>
2023-11-16 13:25:22 +0000

    mclip part 3

--- Chapter 6 - Manipulating Strings/mclip.py
+++ Chapter 6 - Manipulating Strings/mclip.py
@@ -1,13 +1,19 @@
 #! python3
 # mclip.py - A multi-clipboard program.
 
-Text = {'agree': """Yes, I agree. That sounds fine to me.""",
+text = {'agree': """Yes, I agree. That sounds fine to me.""",
     'busy': """Sorry, can we do this later this week or next week?""",
     'upsell': """Would you consider making this a monthly donation?"""}
 
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
     print('Usage: python mclip.py [keyphrase] - copy phrase text')
     sys.exit()
 
 keyphrase = sys.argv[1]     #first command line arg is the keyphrase
+
+if keyphrase in text:
+    pyperclip.copy(text[keyphrase])
+    print(f'Text for {keyphrase} copied to clipboard')
+else:
+    print(f'There is no text for {keyphrase}')
